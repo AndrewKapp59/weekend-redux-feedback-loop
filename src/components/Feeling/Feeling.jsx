e9bca101ebda1c75aebf3b7772f832ca06cb9b94
Andrew Kapp <dkapp59@gmail.com>
2022-02-27 15:21:24 -0600

    Experimenting with material ui

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,10 +1,50 @@
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import './Feeling.css'
+import PropTypes from 'prop-types';
 import Header from '../Header/Header';
+import Button from '@mui/material/Button';
+import Rating from '@mui/material/Rating';
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied';
+import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAltOutlined';
+import SentimentVerySatisfiedIcon from '@mui/icons-material/SentimentVerySatisfied';
+
+const customIcons = {
+  1: {
+    icon: <SentimentVeryDissatisfiedIcon />,
+    label: 'Very Dissatisfied',
+  },
+  2: {
+    icon: <SentimentDissatisfiedIcon />,
+    label: 'Dissatisfied',
+  },
+  3: {
+    icon: <SentimentSatisfiedIcon />,
+    label: 'Neutral',
+  },
+  4: {
+    icon: <SentimentSatisfiedAltIcon />,
+    label: 'Satisfied',
+  },
+  5: {
+    icon: <SentimentVerySatisfiedIcon />,
+    label: 'Very Satisfied',
+  },
+};
+
+function IconContainer(props) {
+  const { value, ...other } = props;
+  return <span {...other}>{customIcons[value].icon}</span>;
+}
+
+IconContainer.propTypes = {
+  value: PropTypes.number.isRequired,
+};
 
 function Feeling() {
-
   const [feeling, setFeeling] = useState('');
 
   const dispatch = useDispatch();
@@ -15,7 +55,7 @@ function Feeling() {
 
     dispatch({
       type: 'ADD_FEELING',
-      payload: {feeling},
+      payload: { feeling },
     });
 
     setFeeling('');
@@ -27,16 +67,30 @@ function Feeling() {
     <section>
       <Header />
       <h1>How are you feeling today?</h1>
-      <form onSubmit={handleFeedbackSubmit}>
+
+      {/* <form onSubmit={handleFeedbackSubmit}>
         <input
           required
           type="number"
           placeholder="0"
           value={feeling}
           onChange={(event) => setFeeling(event.target.value)}
-        />
-        <input value="Next" type="submit" />
-      </form>
+        /> */}
+      {/* <input value="Next" type="submit" /> */}
+      {/* </form> */}
+      
+      <Rating
+        name="highlight-selected-only"
+        defaultValue={3}
+        IconContainerComponent={IconContainer}
+        highlightSelectedOnly
+        onChange ={(event, newValue) => {
+          setFeeling(newValue)
+        }}
+      />
+      <div className='next'>
+      <Button  variant="contained" onClick={handleFeedbackSubmit} >Next</Button>
+      </div>
     </section>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-
+import './Header.css';
 import { useSelector } from 'react-redux';
 
 function Header() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
+    <div className='Feedback'>
+      <header className='Feedback-header'>
+        <h1 className='Feedback-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
     </div>
